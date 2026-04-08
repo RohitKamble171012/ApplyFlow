e2f5f380c2791d869f040119c501cd6395e4053e
Rohit Kamble <rohitkamble171012@gmail.com>
2026-04-08 15:25:10 +0530

    Add debugRedirect route to google.routes.js
    Added debugRedirect route for debugging purposes.

--- server/src/routes/google.routes.js
+++ server/src/routes/google.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { connectGmail, handleCallback, disconnectGmail, getStatus } = require('../controllers/google.controller');
+const { connectGmail, handleCallback, disconnectGmail, getStatus, debugRedirect } = require('../controllers/google.controller');
 const { verifyFirebaseToken } = require('../middleware/auth.middleware');
 
 router.get('/connect', verifyFirebaseToken, connectGmail);
 router.get('/callback', handleCallback);
 router.post('/disconnect', verifyFirebaseToken, disconnectGmail);
 router.get('/status', verifyFirebaseToken, getStatus);
+router.get('/debug-redirect', debugRedirect); // Remove after fixing
 
 module.exports = router;
